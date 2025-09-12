d9a413bb5c79dd29206cfc6b5d7d0b34955fd373
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-12 10:14:15 +0600

    render count

--- src/useRef/Counter.jsx
+++ src/useRef/Counter.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const Counter = () => {
 
+  const [count, setCount] = useState(0)
+  const renderCount = useRef(0)
+
+  renderCount.current = renderCount.current + 1
+
+
   return (
     <div>
-        <p>Count : {}</p>
-        <p>Render Count : {}</p>
-        <button onClick={}>Increase</button>
+        <p>Count : {count}</p>
+        <p>Render Count : {renderCount}</p>
+        <button onClick={()=>setCount(count + 1)}>Increase</button>
     </div>
   )
 }
